48d9716160239e1544b4c12b076bef1412cdd5c6
Mo7mdw <mowageh858@gmail.com>
2025-07-23 00:48:22 +0300

    update the project

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 });
 Route::get('/hello' , function() { return 'hello...' ;
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
